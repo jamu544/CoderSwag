ee6d2d4b3f8d0b418d3207979f6af2bbc896ed64
odwa <odwa.menziwa@jumpco.io>
2022-04-21 12:32:38 +0200

    lesson 8

---
+++ app/src/main/java/android/com/jumpco/io/coderswag/adapters/ProductAdapter.kt
@@ -0,0 +1,39 @@
+package android.com.jumpco.io.coderswag.adapters
+
+import android.com.jumpco.io.coderswag.R
+import android.com.jumpco.io.coderswag.model.Product
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductAdapter(val context: Context, val products: List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_list_item,parent,false)
+        return ProductHolder(view)
+    }
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder?.bindProduct(products[position], context)
+    }
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+    inner class ProductHolder(itemView: View?): RecyclerView.ViewHolder(itemView!!){
+            val productImage = itemView?.findViewById<ImageView>(R.id.productImage)
+            val productName = itemView?.findViewById<TextView>(R.id.productName)
+            val productPrice = itemView?.findViewById<TextView>(R.id.productPrice)
+
+        fun bindProduct(product: Product, context:Context){
+            val resourcId = context.resources.getIdentifier(product.image,"drawable", context.packageName)
+
+            productImage?.setImageResource(resourcId)
+            productName?.text = product.title
+            productPrice?.text = product.price
+        }
+
+    }
+}
--- app/src/main/java/android/com/jumpco/io/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/android/com/jumpco/io/coderswag/controller/ProductsActivity.kt
@@ -1,16 +1,38 @@
 package android.com.jumpco.io.coderswag.controller
 
 import android.com.jumpco.io.coderswag.R
+import android.com.jumpco.io.coderswag.adapters.ProductAdapter
+import android.com.jumpco.io.coderswag.services.DataService
 import android.com.jumpco.io.coderswag.utilities.EXTRA_CATEGORY
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(" testing on click $categoryType")
+        adapter = ProductAdapter(this,DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 4
+        }
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
     }
 }
